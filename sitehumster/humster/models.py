e0477b294a3f723d2d28449595ead417bab0f594
User <You.mail@ru>
2023-11-19 19:58:59 +0300

    таблица

--- sitehumster/humster/models.py
+++ sitehumster/humster/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class humster(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
+
+class Kvantorium(models.Model):
+    surname = models.CharField(max_length=10)
+    name = models.CharField(max_length=10)
+    patronymic = models.CharField(max_length=10, blank=True)
+    Birthday = models.DateTimeField(max_length=15)
+    is_excellent = models.BooleanField(default=True)
+
